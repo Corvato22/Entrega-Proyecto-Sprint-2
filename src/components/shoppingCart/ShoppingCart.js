9cdfaa8defe196070ba2eea0398db2e71dd30f74
Carlos Uribe <89666937+Corvato22@users.noreply.github.com>
2021-11-25 20:10:43 -0500

    Update dev from Corvato (#5)
    * GetData and Links with queryParams
    
    * Fix Routes
    
    * Update detail product
    
    * Fix routes and Nav
    
    * Update Details
    
    * Update Detail
    
    * Update Detail
    
    * Update Detail Flavor
    
    * Fix counter with useContext
    
    * Update useContext Shopping Cart
    
    * Update Shopping Cart
    
    * Depurate Quantitiy Detail
    
    * Update Shopping Cart
    
    * Update Shopping Cart
    
    * Update ShoppingCart
    
    * Update ShoppingCart
    
    * Fix AddToCartButton

--- src/components/shoppingCart/ShoppingCart.js
+++ src/components/shoppingCart/ShoppingCart.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { URL_SHOPPING_CART } from '../../helpers/URL'
+import { ButtonPay } from './ButtonPay'
 
 import { HeaderShoppingCart } from './HeaderShoppingCart'
 
-const StyledCartContainer = styled.div`
+const StyledEmptyCartContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
@@ -17,6 +18,74 @@ const StyledCartContainer = styled.div`
     }
 `
 
+const StyledCartContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 560px;
+`
+
+const StyledCardsContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    margin-bottom: 16px;
+
+
+    img{
+        width: 56px;
+        height: 56px;
+        margin-right: 10px;
+    }
+
+        .containerName{
+        }
+
+        .containerName p:nth-child(1){
+            width: 180px;
+            margin: 0;
+            font-weight: bold;
+            font-size: 14px;
+        }
+
+        .containerName p:nth-child(2){
+            width: 180px;
+            margin: 0;
+            font-style: bold;
+            font-size: 14px;
+            color: #FA4A0C;
+        }
+
+        .containerPrice p:nth-child(1){
+            margin: 0;
+            font-weight: bold;
+            font-size: 16px;
+            color: #FA4A0C;
+        }
+`
+
+const StyledTotalContainer = styled.div`
+    margin-top: 16px;
+    width: 100%;
+    height: 53px;
+    justify-content: space-between;
+    display: flex;
+    background-color: #FFFFFF;
+    border-radius: 20px;
+    align-items: center;
+    padding: 16px;
+
+        h3:nth-child(1){
+            font-size: 17px;
+        }
+
+        h3:nth-child(2){
+            font-size: 17px;
+            color: #FA4A0C;
+        }
+`
+
 export const ShoppingCart = () => {
 
     const [cardsShoppingCart, setCardsShoppingCart] = useState([])
@@ -24,6 +93,7 @@ export const ShoppingCart = () => {
     const getData = async () => {
         let resp = await fetch(URL_SHOPPING_CART)
         let data = await resp.json()
+        console.log(data)
         setCardsShoppingCart(data)
     }
 
@@ -32,26 +102,38 @@ export const ShoppingCart = () => {
         // eslint-disable-next-line
     }, [])
 
-    console.log(cardsShoppingCart[0].products)
-
     return (
         <div>
             <HeaderShoppingCart />
             <StyledCartContainer>
                 {
                     cardsShoppingCart.length === 0 ?
-                        <img src="https://res.cloudinary.com/dzyyi4p7x/image/upload/v1637806696/Guappjolotes%20-%20Sprint%202/NoProducts_a8jouv.png" alt="Empty Cart" />
-                        : cardsShoppingCart[0].products.map((data) => (
-                            <div>
+                        <StyledEmptyCartContainer>
+                            <img src="https://res.cloudinary.com/dzyyi4p7x/image/upload/v1637806696/Guappjolotes%20-%20Sprint%202/NoProducts_a8jouv.png" alt="Empty Cart" />
+                        </StyledEmptyCartContainer>
+
+                        : cardsShoppingCart.map((data) => (
+                            <StyledCardsContainer key={data.id}>
                                 <img src={data.image} alt={data.name} />
-                                <div>
-                                    <h3>{data.name}</h3>
-                                    <h3>x1</h3>
+                                <div className="containerName">
+                                    <p>{data.name}</p>
+                                    <p>x1</p>
                                 </div>
-                                <p>{'$' + data.price} MXN</p>
-                            </div>
+                                <div className="containerPrice">
+                                    <p>{'$' + data.price} MXN</p>
+                                </div>
+                            </StyledCardsContainer>
                         ))
                 }
+                {
+                    cardsShoppingCart.length === 0 ?
+                        null
+                        : <StyledTotalContainer>
+                            <h3>Total</h3>
+                            <h3>$ {cardsShoppingCart.reduce((acc, elem) => acc + elem.price, 0)} MXN</h3>
+                        </StyledTotalContainer>
+                }
+                <ButtonPay cardsShoppingCart={cardsShoppingCart} />
             </StyledCartContainer>
         </div>
     )
